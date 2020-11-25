9feb3d00d6e65773e62361dacc51ac7749088314
Seby <sebywolfpack@gmail.com>
2020-11-25 08:38:35 -0500

    Initial room tunnel

--- BuildCaveRoom.lua
+++ BuildCaveRoom.lua
@@ -2,6 +2,9 @@ local args = {...}
 
 local tunneltoroom = args[1]
 local roomsize = args[2]
+local roomheight = 3
+
+local startY = 0
 
 for i=1,tunneltoroom,1 do
     even = i - math.floor(i/2)*2
@@ -13,6 +16,7 @@ for i=1,tunneltoroom,1 do
         turtle.up()
         turtle.dig()
         turtle.forward()
+        startY = startY + 2
     else
         turtle.dig()
         turtle.down()
@@ -20,5 +24,40 @@ for i=1,tunneltoroom,1 do
         turtle.down()
         turtle.dig()
         turtle.forward()
+        startY = startY - 2
     end
 end
+
+for i=1,startY,1 do
+    turtle.down()
+end
+
+turtle.dig()
+turtle.forward()
+for i=1,roomheight,1 do
+    turtle.digUp()
+    turtle.up()
+end
+
+for i=1,roomsize,1 do
+    even = i - math.floor(i/2)*2
+
+    turtle.dig()
+    turtle.forward()
+
+    if even ~= 0 then
+        for j=1,roomheight,1 do
+            turtle.digDown()
+            turtle.down()
+        end
+        turtle.forward()
+    else
+        for j=1,roomheight,1 do
+            turtle.digUp()
+            turtle.up()
+        end
+        turtle.forward()
+    end
+end
+
+
