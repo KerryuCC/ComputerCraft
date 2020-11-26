1fe34867f8f946d3dd6ae0a2ce3329c6880a08ff
Seby <sebywolfpack@gmail.com>
2020-11-26 05:49:28 -0500

    Should work

--- BuildCaveRoom.lua
+++ BuildCaveRoom.lua
@@ -81,32 +81,54 @@ else
     end
 end
 
-turtle.left()
+turtle.turnLeft()
 turtle.dig()
-
+turtle.forward()
 
 for i=1,roomsize,1 do
     even = i - math.floor(i/2)*2
 
     for j=1,halfroom,1 do
-        turtle.dig()
-        turtle.forward()
+        evena = j - math.floor(j/2)*2
 
-        if even ~= 0 then
-            for j=1,roomheight,1 do
-                turtle.digDown()
-                turtle.down()
-                startY = startY - roomheight
-            end
-            turtle.forward()
-        else
+        if evena ~= 0 then
             for j=1,roomheight,1 do
                 turtle.digUp()
                 turtle.up()
                 startY = startY + roomheight
             end
-            turtle.forward()
+        else
+            for j=1,roomheight,1 do
+                turtle.digDown()
+                turtle.down()
+                startY = startY - roomheight
+            end
         end
+        
+        turtle.dig()
+        turtle.forward()
+    end
+
+    if startY > 0 then
+        for i=1,startY,1 do
+            turtle.down()
+        end
+    else
+        for i=startY,0,1 do
+            turtle.up()
+        end
+    end
+
+    if even ~= 0 then
+        turtle.turnLeft()
+        turtle.dig()
+        turtle.forward()
+        turtle.turnLeft()
+    else
+        turtle.turnRight()
+        turtle.dig()
+        turtle.foward()
+        turtle.turnRight()
     end
 end
 
