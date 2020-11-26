87a7d4b427b5d52076eed319c4752cb28f652e49
Seby <sebywolfpack@gmail.com>
2020-11-26 05:15:27 -0500

    Testchanges

--- BuildCaveRoom.lua
+++ BuildCaveRoom.lua
@@ -2,6 +2,7 @@ local args = {...}
 
 local tunneltoroom = args[1]
 local roomsize = args[2]
+local halfroom = math.ceil(roomsize/2)
 local roomheight = 3
 
 local startY = 0
@@ -28,8 +29,14 @@ for i=1,tunneltoroom,1 do
     end
 end
 
-for i=1,startY,1 do
-    turtle.down()
+if startY > 0 then
+    for i=1,startY,1 do
+        turtle.down()
+    end
+else
+    for i=startY,0,1 do
+        turtle.up()
+    end
 end
 
 turtle.dig()
@@ -39,7 +46,7 @@ for i=1,roomheight,1 do
     turtle.up()
 end
 
-for i=1,roomsize,1 do
+for i=1,roomsize-1,1 do
     even = i - math.floor(i/2)*2
 
     turtle.dig()
@@ -49,15 +56,58 @@ for i=1,roomsize,1 do
         for j=1,roomheight,1 do
             turtle.digDown()
             turtle.down()
+            startY = startY - roomheight
         end
         turtle.forward()
     else
         for j=1,roomheight,1 do
             turtle.digUp()
             turtle.up()
+            startY = startY + roomheight
         end
         turtle.forward()
     end
 end
+
+if startY > 0 then
+    for i=1,startY,1 do
+        turtle.down()
+    end
+else
+    for i=startY,0,1 do
+        turtle.up()
+    end
+end
+
+turtle.left()
+turtle.dig()
+
+
+for i=1,roomsize,1 do
+    even = i - math.floor(i/2)*2
+
+    for j=1,halfroom,1 do
+        turtle.dig()
+        turtle.forward()
+        
+        if even ~= 0 then
+            for j=1,roomheight,1 do
+                turtle.digDown()
+                turtle.down()
+                startY = startY - roomheight
+            end
+            turtle.forward()
+        else
+            for j=1,roomheight,1 do
+                turtle.digUp()
+                turtle.up()
+                startY = startY + roomheight
+            end
+            turtle.forward()
+        end
+    end
+end
+
+-- Do the opposite side here
 
 
